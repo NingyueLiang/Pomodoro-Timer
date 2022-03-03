cd896d5183af43669354f2cde6ab9f1e9779cdab
Thomas Cornell <tjcornell@wustl.edu>
2022-03-03 17:17:22 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../styles/globals.css'
-import initAuth from '../initAuth' // initalize Firebase authentication; source: https://github.com/gladly-team/next-firebase-auth#get-started
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-
-initAuth()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
