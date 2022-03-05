a316fb6c173fabae933d0b0f7205a24ec7d8e995
Thomas Cornell <tjcornell@wustl.edu>
2022-03-05 17:23:11 -0600

    minor Timer tweaks

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -44,17 +44,13 @@ const Timer = (props) => {
 
   return (
     <div className="timer">
-      {minutes === 0 && seconds === 0 ? null : (
-        <h1>
-          {" "}
-          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-        </h1>
-      )}
+      <h1>
+        {" "}
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </h1>
       <button onClick={handleStart}>Start</button>
       <button onClick={handlePause}>Stop</button>
       <button onClick={handleReset}>Reset</button>
-
-
     </div>
   );
 };
