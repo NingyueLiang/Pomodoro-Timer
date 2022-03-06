a91a59948b98c5924ac3c74ccd7404e7e54fd6b8
Im-possibleYi <63691217+Im-possibleYi@users.noreply.github.com>
2022-03-06 14:18:14 -0600

    Version with QR delay 1 sec

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -5,38 +5,54 @@ import { useRouter } from 'next/router'
 
 const Timer = (props) => {
   const { initialMinutes = 25, initialSeconds = 0 } = props;
+  // const initialTimestamp = 25 * 60;
+  // const {prevStartTime = Date.now()} = props;
+  // console.log(prevStartTime);
+  // const mills = Date.now() - prevStartTime;
+  // const diff = Math.floor(mills / 1000);
+  // initialTimestamp = initialTimestamp - diff;
+  // const initialMinutes = parseInt(initialTimestamp / 60);
+  // const initialSeconds = Math.round(initialTimestamp % 60);
+
+  console.log(initialMinutes, initialSeconds);
+
   const [minutes, setMinutes] = useState(initialMinutes);
   const [seconds, setSeconds] = useState(initialSeconds);
   const [isCountingDown, setIsCountingDown] = useState(false);
 
   const [qrVisible, setQRVisible] = useState(false);
   const [qrValue, setQRValue] = useState('');
+  const [startTime, setStartTime] = useState(0);
 
   const router = useRouter();
 
   useEffect(() => {
     const countdownInterval = setInterval(() => {
-      if (props.isQR || isCountingDown){
+      if (props.isQR || isCountingDown) {
         if (seconds > 0) {
           setSeconds(seconds - 1);
         }
         else if (seconds == 0) {
           if (minutes == 0) {
             clearInterval(countdownInterval);
-          } 
+          }
           else {
             console.log('test', props.isQR, seconds, minutes)
             setMinutes(minutes - 1);
             setSeconds(59);
           }
         }
-    }}, 1000);
+      }
+    }, 1000);
     return () => {
       clearInterval(countdownInterval);
     };
   });
 
   const handleStart = () => {
+    // var myDate = new Date();
+    // const startTime = Date.now();
+    setStartTime(Date.now());
     setIsCountingDown(true);
   }
 
@@ -49,13 +65,14 @@ const Timer = (props) => {
     setSeconds(initialSeconds);
   }
 
-  const handleCreateQRCode = () => {
-    handlePause();
-
-    const baseURL = 'https://foocus.vercel.app/qr';
-    // const strValue = '?minutes=' + minutes + '&seconds=' + seconds;
+  const handleCreateQRCode = async () => {
+    // handlePause();
+    // countdownInterval();
+    const baseURL = 'http://localhost:3002/qr';
     const isQR = true;
-    const strValue = '?minutes=' + minutes + '&seconds=' + seconds + '&isQR=' + isQR;
+    // const strValue = '?minutes=' + minutes + '&seconds=' + seconds + '&isQR=' + isQR;
+    const strValue = '?startTime=' + startTime + '&isQR=' + isQR;
+
     console.log(baseURL + strValue);
     setQRValue(baseURL + strValue);
 
@@ -64,17 +81,32 @@ const Timer = (props) => {
 
   const toggleShowQR = () => {
     setQRVisible(!qrValue);
+    if (isCountingDown){
+    let initialTimestamp = 25 * 60;
+    const prevStartTime = startTime;
+    // const mills = 1646593098423 - prevStartTime;
+    const mills = Date.now() - prevStartTime;
+
+    // console.log(Date.now());
+    const diff = mills / 1000;
+    // const diff = 40;
+    initialTimestamp = initialTimestamp - diff;
+    const resetlMinutes = parseInt(initialTimestamp / 60);
+    const resetSeconds = Math.round(initialTimestamp % 60);
+    setMinutes(resetlMinutes);
+    setSeconds(resetSeconds);
+  }
   }
 
   return (
-    <div className="timer">
+<div className="timer">
       {qrVisible &&
         <>
           <QRCode value={qrValue} />
           <button onClick={toggleShowQR}>Show Timer</button>
         </>
       }
-      {!qrVisible && 
+      {!qrVisible &&
         <>
           <h1>
             {" "}
--- pages/qr.js
+++ pages/qr.js
@@ -9,10 +9,23 @@ import { useRouter } from 'next/router'
 const Qr = ({query}) => {
     const router = useRouter();
 
+    let initialTimestamp = 25 * 60;
+    const prevStartTime = router.query.startTime;
+    const mills = Date.now() - prevStartTime;
+
+    // console.log(Date.now());
+    const diff = mills / 1000;
+    // const diff = 40;
+    initialTimestamp = initialTimestamp - diff;
+    const initialMinutes = parseInt(initialTimestamp / 60);
+    const initialSeconds = Math.round(initialTimestamp % 60);
+
     return(
         <div>
         <Header />
-        {router.isReady && <Timer initialMinutes={router.query.minutes} initialSeconds={router.query.seconds} isQR={router.query.isQR}/>}
+        {router.isReady && <Timer initialMinutes={initialMinutes} initialSeconds={initialSeconds} isQR={router.query.isQR}/>}
+        {/* {router.isReady && <Timer prevStartTime={router.query.startTime} isQR={router.query.isQR}/>} */}
+        
         <Footer />
       </div>
     );
